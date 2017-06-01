f524e3e5efe82a8e283a8fc337a0898187455f19
ashaffir <alfredshaffir@alfreds-MacBook.local>
2017-06-01 16:29:50 +0300

    initial

---
+++ aliases.sh
@@ -0,0 +1,30 @@
+echo "SETTING ALIASES"
+alias a=alias 
+a h='history | cut -c 8-'
+a la='ls -la'
+a ll='ls -l'
+a p=pwd
+a lr='ls -lrt'
+a m=more
+a l=less
+a mr='more README'
+a cl=clear
+a pt2=python2
+a pt=python3
+a atm=atom
+a e=emacs
+a mir='more ~/README'
+alias me='m ~/emacs.txt'
+alias via='vi ~/.bash_aliases'
+alias vir='vi ~/README'
+a ..='cd ../'
+a ...='cd ../../'
+a cw='ssh -Y alfreds@104.45.231.235 -p22'
+a cdr='cd /Users/alfredshaffir/Documents/python-git'
+a mu='m ~/used_commands.txt'
+a n=nano
+a mig='more ~/git.txt'
+a cv='ssh -L 5901:127.0.0.1:5901 alfreds@104.45.231.235'
+a cj='ssh -L 5901:127.0.0.1:5901 alfreds@13.91.88.219'
+a myip='curl ipinfo.io/ip'
+
