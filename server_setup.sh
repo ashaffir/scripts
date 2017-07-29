c213aec7e69dfacbf5f9c44e7ce5aa5f5f868039
ashaffir <alfredshaffir@alfreds-MacBook.local>
2017-07-29 12:43:30 +0300

    changes

--- server_setup.sh
+++ server_setup.sh
@@ -7,6 +7,8 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install ubuntu-desktop
 sudo apt-get install xfce4 xfce4-goodies tightvncserver
+vncserver
+vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 cp scripts/xstartup ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
@@ -17,6 +19,7 @@ sudo service vncserver start
 #make sure all services are running
 sudo /opt/lampp/lampp start
 
+# Installing the environment with XAMPP
 #After doanloading the XAMPP from the ubuntu website:
 sudo wget https://www.apachefriends.org/xampp-files/7.1.4/xampp-linux-x64-7.1.4-0-installer.run
 
@@ -26,6 +29,13 @@ sudo wget https://www.apachefriends.org/xampp-files/5.6.30/xampp-linux-x64-5.6.3
 
 sudo chmod +x xampp-linux-x64-5.6.30-1-installer.run 
 sudo ./xampp-linux-x64-5.6.30-1-installer.run 
+
+# OR
+sudo apt-get install apache2
+sudo apt-get install php5
+sudo apt-get mysql-server
+sudo apt-get php5-mysql
+sudo /etc/init.d/apache2 restart
 
 # If requred DVWA:
 wget http://www.computersecuritystudent.com/SECURITY_TOOLS/DVWA/DVWAv107/lesson1/DVWA-1.0.7.zip
@@ -47,4 +57,6 @@ pip3 install git+git://github.com/davispuh/MySQL-for-Python-3
 sudo pip install virtualenv
 sudo virtualenv venv
 
+# Enable locate command
+sudo updatedb && locate -e bench-repo
 
