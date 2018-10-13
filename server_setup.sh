3cea9270a88441c7346b85bb72605164af7f140b
alfred shaffir <alfredshaffir@alfreds-macbook.mynet>
2018-10-13 13:22:49 +0300

    s

--- server_setup.sh
+++ server_setup.sh
@@ -6,15 +6,18 @@ sudo git clone https://github.com/ashaffir/scripts.git
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install ubuntu-desktop
-sudo apt-get install xfce4 xfce4-goodies tightvncserver
+sudo apt-get install vnc4server
+sudo apt-get install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+#sudo apt-get install xfce4 xfce4-goodies tightvncserver
 vncserver
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 cp scripts/xstartup ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
-sudo cp scripts/vncserver /etc/init.d/vncserver
-sudo chmod +x /etc/init.d/vncserver
-sudo service vncserver start
+# sudo cp scripts/vncserver /etc/init.d/vncserver
+# sudo chmod +x /etc/init.d/vncserver
+# sudo service vncserver start
+vncserver -geometry 1280x900 -depth 24
 
 #make sure all services are running
 sudo /opt/lampp/lampp start
