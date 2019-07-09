8c8440f9206ce442b7cb32c1e1de14c7bee4ceb4
Alfred Shaffir <alfred@iolite.io>
2019-07-09 18:54:49 +0300

    additions

--- vnc_shit.sh
+++ vnc_shit.sh
@@ -1,3 +1,19 @@
 sudo rm /tmp/.X1-lock
 sudo rm /tmp/.X11-unix/X1
 vncserver :1
+
+Fix the TAB completion issue:
+edit
+~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
+find the line 
+
+<property name="&lt;Super&gt;Tab" type="string" value="switch_window_key"/>
+
+and change it to 
+
+<property name="&lt;Super&gt;Tab" type="empty"/>
+
+restart the VNC session.
+
+
