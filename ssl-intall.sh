d574946eaaac4d689ee2084e13a9cc5f510555dd
ashaffir <ashaffir@gmail.com>
2020-03-21 09:58:37 +0200

    updates

--- ssl-intall.sh
+++ ssl-intall.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
-#########################################
-# To Run:
-# ssl-install.sh <website URL>
-#
-#########################################  
-cd /usr/local/sbin
-wget https://dl.eff.org/certbot-auto
-chmod a+x /usr/local/sbin/certbot-auto
-apt-get install python-letsencrypt-apache
-sudo apt-get update
-apt-get install python3-certbot-apache
-add-apt-repository ppa:certbot/certbot
-apt-get update
-apt-get install python-certbot-apache
-sudo certbot --apache -d $1 -d www.$1
+Check this site for client ptions: https://letsencrypt.org/docs/client-options/
 
-echo "END OF SCRIPT"
+This looks like the best solution: 
+https://certbot.eff.org/
+
+
